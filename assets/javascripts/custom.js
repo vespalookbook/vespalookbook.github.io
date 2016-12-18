9b16778c33ed16baca4f4c8ab3f5f16e999428a9
Cenk <cenk@hunili.com>
2016-12-18 23:45:54 +0200

    instafeed

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
-    var feed = new instafeed({
+    var feed = new Instafeed({
         get: 'user',
         userId: '4112109893',
         accessToken: '73b25502a1c24cd4aa8aac6547951ab8'
