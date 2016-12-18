7b3ce1af796d0b680d56d6389fed2bcca88a1f52
Cenk <cenk@hunili.com>
2016-12-19 00:48:53 +0200

    instafeed

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -1,8 +1,21 @@
 // $(document).ready(function() {
-    var feed = new Instafeed{
-        get: 'user',
-        userId: '4112109893',
-        accessToken: '56c79e7c896b43dda4ff9f7536174232'
-    }
-    feed.run();
+    // var feed = new Instafeed({
+        // get: 'user',
+        // userId: '4112109893',
+        // accessToken: '56c79e7c896b43dda4ff9f7536174232'
+    // });
+    // feed.run();
 // });
+
+jQuery.fn.spectragram.accessData = {
+    accessToken: '56c79e7c896b43dda4ff9f7536174232',
+    clientID: '905f647d09e2489da0991cdea08ea334'
+};
+
+$('div').spectragram({
+    query: 'converse',
+    max: 14,
+    size: 'big',
+    wrapEachWith: '<p></p>',
+    complete: alert('Done!');
+});
