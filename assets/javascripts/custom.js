fb7468dd6ea7ca41ade2098a0bd6134fa36c77ea
Cenk <cenk@hunili.com>
2016-12-18 23:42:32 +0200

    instafeed

--- assets/javascripts/custom.js
+++ assets/javascripts/custom.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	$( document ).ready(function() {
 	    var feed = new Instafeed({
 	        get: 'user',
-	        userId: '4112109893'
+	        userId: '4112109893',
+	        accessToken: '73b25502a1c24cd4aa8aac6547951ab8'
 	    });
 	    feed.run();
 	});
